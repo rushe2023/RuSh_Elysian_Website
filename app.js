de94afbf727082456d540f16215303f82881a4d0
rushe2023 <143425621+rushe2023@users.noreply.github.com>
2023-11-22 20:59:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,45 +3,52 @@ import bodyParser from "body-parser";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
 import path from "path";
-
+ 
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
-
+ 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+ 
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-
+ 
 app.get("/about", (req, res) => {
   res.render("about.ejs");
 });
-
+ 
 app.get("/contact", (req, res) => {
   res.render("contact.ejs");
 });
-
+ 
 app.get("/service", (req, res) => {
   res.render("services.ejs");
 });
-
-
+ 
+app.get("/Blogs", (req, res) => {
+  res.render("Blogs.ejs");
+});
+ 
+app.get("/FlexBlog", (req, res) => {
+  res.render("FlexBlog.ejs");
+});
+ 
 //Mailchimp api code
 app.use(express.json()); // Parse JSON request bodies
-
+ 
 // Handle form submissions
 app.post('/subscribe', async (req, res) => {
   try {
     const apiKey = "a13dba0cf4d0c949ea09a9e1ecb3af8f-us8";
     //process.env.MAILCHIMP_API_KEY
-    const listId = "8b8bf9d05f" ; 
-    //process.env.MAILCHIMP_LIST_ID 
-
+    const listId = "8b8bf9d05f" ;
+    //process.env.MAILCHIMP_LIST_ID
+ 
     // Get the form data
     const { name, email, subject, message } = req.body;
-
+ 
     // Create the JSON data for the Mailchimp API request
     const data = {
       email_address: email,
@@ -62,9 +69,9 @@ app.post('/subscribe', async (req, res) => {
       },
       body: JSON.stringify(data),
     });
-
+ 
     const responseData = await response.json();
-
+ 
     // Handle the Mailchimp API response
     if (responseData.status === 'subscribed') {
       // Successful subscription
@@ -79,13 +86,8 @@ app.post('/subscribe', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
-
+ 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
